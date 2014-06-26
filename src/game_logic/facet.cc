1d86dd9b29a7fcd35004ecd1b74249f466683b8f
Ben Lewis <ben.lewis@gmail.com>
2014-06-25 22:59:10 -0700

    Facets

---
+++ src/game_logic/facet.cc
@@ -0,0 +1,13 @@
+//
+//  facet.cpp
+//  osx_unhinged
+//
+//  Created by Ben Lewis on 6/24/14.
+//  Copyright (c) 2014 ben. All rights reserved.
+//
+
+#include "game_logic/facet.h"
+
+Facet::Facet() {
+
+}
---
+++ src/game_logic/facet.h
@@ -0,0 +1,23 @@
+//
+//  facet.h
+//  osx_unhinged
+//
+//  Created by Ben Lewis on 6/24/14.
+//  Copyright (c) 2014 ben. All rights reserved.
+//
+
+#ifndef __osx_unhinged__facet__
+#define __osx_unhinged__facet__
+
+#include "game_logic/piece.h"
+#include "render/material.h"
+
+class Facet {
+private:
+
+public:
+  Facet();
+
+};
+
+#endif /* defined(__osx_unhinged__facet__) */
